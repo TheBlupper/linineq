84c23622dbc5381b9c646ef554f6d48ebe8a503a
blupper <33233478+TheBlupper@users.noreply.github.com>
2024-05-07 12:25:28 +0200

    Add divisibility example

---
+++ example_divisibility.py
@@ -0,0 +1,33 @@
+from sage.all import *
+from Crypto.Util.number import bytes_to_long
+from linineq import solve_ineq
+
+# problem posed by @neobeo on discord:
+# what's the smallest integer i such that
+# bytes_to_long(str(i).encode()) % 13**37 == 0
+
+# this does not find *the* smallest i, that is unsolved
+# afaik. this only finds a single solution which is
+# 50 digits long, but you can go further by just throwing
+# more computing power at it
+
+# the best i've managed is 42 digits (with lp_bound=2):
+# 375100768340560904583476889463350224435184
+
+ndig = 50
+
+L = identity_matrix(ndig)
+for i in range(ndig-1):
+    L[i, i+1] = -256
+L[0,0] = 13**37
+L = L[::-1, :]
+
+zero = ord('0')
+Mineq = L.stack(-L)
+bineq = vector([zero]*ndig + [-(zero+9)]*ndig)
+bineq[0] = zero+1 # first digit can't be 0
+
+sol = solve_ineq(Mineq, bineq, lp_bound=1)
+n = int(bytes(L*sol))
+print(n)
+assert bytes_to_long(str(n).encode()) % 13**37 == 0
