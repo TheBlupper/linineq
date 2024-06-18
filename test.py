3940888440d8eddf173efc7a8175917dd30caa2b
blupper <33233478+TheBlupper@users.noreply.github.com>
2024-06-18 18:22:35 +0200

    Add "tests"

---
+++ test.py
@@ -0,0 +1,18 @@
+from sage.all import *
+from linineq import *
+from itertools import product
+
+reds = [wBKZ(), wLLL(), wflatter()]
+cvps = [wbabai_cvp(), wfplll_cvp()] + [wkannan_cvp(reduce=red) for red in reds]
+
+mask = 3473400794307473
+m = 1 << 52
+b = 4164880461924199
+a = 2760624790958533
+n = 16
+
+lb = [9 * (1 << 52)//10]*n
+ub = [m-1]*n
+
+for cvp, red in product(cvps, reds):
+    assert {s[0] for s in solve_bounded_lcg_gen(a, b, m, lb, ub, reduce=red, cvp=cvp)} == {4337090406850605, 4359404241011232}
