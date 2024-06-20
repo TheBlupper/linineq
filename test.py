c7c9798ba859f8e420cd12600bcd471b9db5aaf3
blupper <33233478+TheBlupper@users.noreply.github.com>
2024-06-20 12:05:16 +0200

    cvp test

--- test.py
+++ test.py
@@ -14,5 +14,12 @@ n = 16
 lb = [9 * (1 << 52)//10]*n
 ub = [m-1]*n
 
+B = random_matrix(ZZ, 50, 100)
+t = random_vector(100, 2000)
+for cvp in cvps:
+    u, v = cvp(B, t, coords=True)
+    assert u == v*B
+
+
 for cvp, red in product(cvps, reds):
     assert {s[0] for s in solve_bounded_lcg_gen(a, b, m, lb, ub, reduce=red, cvp=cvp)} == {4337090406850605, 4359404241011232}
